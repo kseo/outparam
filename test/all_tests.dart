106dcdac206bc5e6b86c7da3f061ee90535764dc
Kwang Yul Seo <kwangyul.seo@gmail.com>
2015-12-09 12:37:48 +0900

    Add all_tests.dart to automate tests on Travis.

---
+++ test/all_tests.dart
@@ -0,0 +1,10 @@
+// Copyright (c) 2015, Kwang Yul Seo. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+library outparam.test;
+
+import 'outparam_test.dart' as outparam_test;
+
+void main() {
+  outparam_test.main();
+}
