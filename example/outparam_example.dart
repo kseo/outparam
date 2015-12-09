5529ed7184d40afc93b96c1a4efebfc7250ec9c6
Kwang Yul Seo <kwangyul.seo@gmail.com>
2015-12-09 12:28:17 +0900

    Add an example.

--- example/outparam_example.dart
+++ example/outparam_example.dart
@@ -1,11 +1,26 @@
-// Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
+// Copyright (c) 2015, Kwang Yul Seo. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 library outparam.example;
 
 import 'package:outparam/outparam.dart';
 
+void testString(String s, OutParam<bool> containsPeriod,
+    OutParam<bool> containsComma, OutParam<bool> containsSemicolon) {
+  containsPeriod.value = s.contains('.');
+  containsComma.value = s.contains(',');
+  containsSemicolon.value = s.contains(';');
+}
+
 main() {
-  var awesome = new Awesome();
-  print('awesome: ${awesome.isAwesome}');
+  final containsPeriod = new OutParam<bool>();
+  final containsComma = new OutParam<bool>();
+  final containsSemicolon = new OutParam<bool>();
+
+  testString(
+      'Hello, World.', containsPeriod, containsComma, containsSemicolon);
+
+  print(containsPeriod.value); // true
+  print(containsComma.value); // true
+  print(containsSemicolon.value); // false
 }
